64a54d535e9d0227b668d9a59f024d33352f906d
Ochs <jochs4fun@gmail.com>
2022-07-07 18:43:35 -0400

    Added menu item functionality
    A static list is used to add items to a menu div

--- src/menu.js
+++ src/menu.js
@@ -7,24 +7,48 @@ const holder = () => {
 }
 
 
-const content = (div) => {
+const h1content = (div) => {
     let hOne = document.createElement("h1");
     hOne.classList.add("menu");
     hOne.innerText = "Menu";
     div.appendChild(hOne)
 
-    let pOne  = document.createElement("p");
-    pOne.classList.add("menu-item");
-    pOne.innerText = "Lasnaga";
-    div.appendChild(pOne)
-
     return div
 }
 
+let menuArray = [
+    ["Lasagna", 15.99],
+    ["Spaghetti", 16.99],
+    ["Veal", 16.99],
+    ["Burger", 12.99],
+];
+
+const appendMenuItems = (lt, div) => {
+
+    let itemHldr = document.createElement("div")
+    itemHldr.innerText = '';
+    itemHldr.classList.add("menuItemDiv")
+
+    lt.forEach(element => {
+        
+        let food = element[0];
+        let price = element[1];
+        console.log(food)
+        itemHldr.innerHTML += `
+        <a id="menuItem">
+            <div id="item">
+                <p id="food"> ${food}</p>
+                <p id="price">${price}</p>
+            </div>
+        </a>`;});
+    div.appendChild(itemHldr)
+    return div
+}
 
 const loadMenu = () => {
     let bandiv = holder();
-    content(bandiv);
+    h1content(bandiv);
+    appendMenuItems(menuArray, bandiv);
     return bandiv;
 }
 
